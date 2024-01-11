f0093ac98d0a58981dee35a53415c2fc7272fdb2
souvikotaku <manson1989@>
2024-01-11 11:21:41 +0530

    chnges

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const BarChart = () => {
+  return <div>BarChart</div>;
+};
+
+const Dashboard = () => {
+  return (
+    <h1>
+      Dashboard <BarChart />
+    </h1>
+  );
+};
+
+export default Dashboard;
